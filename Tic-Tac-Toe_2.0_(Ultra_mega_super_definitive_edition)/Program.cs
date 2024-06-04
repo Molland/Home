a17331fdf5bf2cde86d9e4a94bd2abb07e344a7f
Molland <foxgamer067354@gmail.com>
2024-06-04 03:02:27 +0300

    first commit

---
+++ Tic-Tac-Toe_2.0_(Ultra_mega_super_definitive_edition)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe_2._0__Ultra_mega_super_definitive_edition_
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
